3269669ec255786046578476c8bfb362cf6d943c
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-03-25 02:18:08 +0500

    Create unit and integration tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Group.new({ name: 'Mortgage', icon: 'https://www.smashbros.com/wiiu-3ds/images/character/toon_link/main.png',
+                user_id: 1 })
+  end
+
+  before { subject.save }
+
+  it 'Name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'icon should be present' do
+    subject.icon = nil
+    expect(subject).to_not be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new({ name: 'David', email: 'david@gmail.com', password: '1234567', password_confirmation: '1234567' })
+  end
+
+  before { subject.save }
+
+  it 'Name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'password should be present' do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Password must have length greater then 6' do
+    subject.password = '12345'
+    expect(subject).to_not be_valid
+  end
 end
